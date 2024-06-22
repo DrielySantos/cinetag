885526ec90ad6207eebbb1449134a7c0031b0455
Driely Santos <santosdriely98@gmail.com>
2024-06-22 19:00:51 -0300

    Rotas aninhadas

--- src/routes.js
+++ src/routes.js
@@ -1,30 +1,23 @@
-import Cabecalho from "components/Cabecalho";
-import Container from "components/Container";
-import Rodape from "components/Rodape";
-import FavoritosProvider from "contextos/Favoritos";
 import Favoritos from "pages/Favoritos";
 import Inicio from "pages/Inicio";
 import NaoEncontrada from "pages/NaoEncontrada";
+import PaginaBase from "pages/PaginaBase";
 import Player from "pages/Player";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function appRoutes(){
-    return(
-        <BrowserRouter>
-            <Cabecalho/>
-            <Container>
-                <FavoritosProvider>
-                    <Routes>
-                        <Route path="/" element={<Inicio />}></Route>
-                        <Route path="/favoritos" element={<Favoritos />}></Route>
-                        <Route path="/:id" element={<Player />}></Route>
-                        <Route path="*" element={<NaoEncontrada />}></Route>
-                    </Routes>
-                </FavoritosProvider>
-            </Container>
-            <Rodape />
-        </BrowserRouter>
-    )
+function appRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaginaBase />}>
+          <Route index element={<Inicio />}></Route>
+          <Route path="favoritos" element={<Favoritos />}></Route>
+          <Route path=":id" element={<Player />}></Route>
+          <Route path="*" element={<NaoEncontrada />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default appRoutes;
